f6e389ffbf19448e0e8fdd443f2129f17f349459
front-end-captain <18810940932@163.com>
2018-10-23 15:18:20 +0800

    :sparkles: ICO DApp 基本页面布局

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import withRoot from "../libs/withRoot";
+import Layout from "../components/Layout.js";
+
+class Index extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Button variant="raised" color="primary">
+          Welcome to Ethereum ICO DApp!
+        </Button>
+      </Layout>
+    );
+  }
+}
+
+export default withRoot(Index);
---
+++ server.js
@@ -0,0 +1,14 @@
+const next = require("next");
+const http = require("http");
+const Chalk = require("chalk");
+
+const routes = require("./routes");
+
+const app = next({ dev: process.env.NODE_ENV !== "production" });
+const handler = routes.getRequestHandler(app);
+
+app.prepare().then(() => {
+  http.createServer(handler).listen(3000, () => {
+    console.log( Chalk.green("server started on port: 3000"));
+  });
+});
